6679a6f7242df88651c011a73baa83ed66a9b671
Gustavo Rivera Pecunia <grpecunia@gmail.com>
2020-03-03 10:26:44 -0500

    finished the intro to node.js lab

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  
+  constructor() {
+    this.items = [],
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b) => a - b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    
+  if (pos > this.items.length ) {
+  throw new Error("OutOfBounds");   
+  }
+  return this.items.indexOf(pos);
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+        throw new Error("EmptySortedList");
+    } 
+    // this.items.sort((a,b) => a - b)
+    return this.items[Number(this.items.length) - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+        throw new Error();
+    } else {
+      // this.items.sort((a, b) => a - b);
+      return this.items[0]
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length > 0) {
+      return this.items.reduce((a, b) => a + b, 0);
+    }
+    return 0
+  }
 
-  avg() {}
+  avg() {
+   if (this.items.length > 0) {
+     let sum = this.items.reduce((a, b) => a + b, 0);
+     let den = this.items.length;
+     return sum / den;
+   }
+        throw new Error();
+
+  }
 }
 
 module.exports = SortedList;
